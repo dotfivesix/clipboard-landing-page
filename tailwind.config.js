f1af89befe509486b876558ea3dc4ff860492469
DarkDev <mda.business.on@gmail.com>
2022-12-27 21:00:13 +0500

    Setup File Folder and Structure

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        "bai": "'Bai Jamjuree', sans-serif"
+      },
+      colors: {
+        "strong-cyan": "hsl(171, 66%, 44%)",
+        "light-blue": "hsl(233, 100%, 69%)",
+        "dark-grayish-blue": "hsl(210, 10%, 33%)",
+        "grayish-blue": "hsl(201, 11%, 66%)"
+      }
+    },
+  },
+  plugins: [],
+}
